005b792ab5f2a5ab58141210e2484d78219bf664
Mr.Hugo <mosieur.hugo@gmail.com>
2017-12-06 16:54:17 +0100

    add the crawl method that get all links in a page from an url

--- ddd/src/test/java/services/CrawlerService.java
+++ ddd/src/test/java/services/CrawlerService.java
@@ -1,14 +1,39 @@
 package services;
 
 import models.Url;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.io.*;
 
-/**
- * Created by val on 06/12/17.
- */
 public class CrawlerService {
-    public ArrayList<Url> crawl(Url url) {
-        return null;
+
+    public String getDomainName(String url) throws URISyntaxException {
+        URI uri = new URI(url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public ArrayList<Url> Crawl(Url url) throws IOException, URISyntaxException {
+        Document doc = Jsoup.connect(url.getValue()).get();
+        Elements links = doc.select("a");
+
+        ArrayList<Url> listUrl = new ArrayList<Url>();
+
+        for (Element l : links) {
+            String linkValue = l.attr("href");
+            if (linkValue.substring(0, 1).equals("/")) {
+                linkValue = "http://" + this.getDomainName(url.getValue()) + linkValue;
+            }
+            listUrl.add(new Url(linkValue));
+            System.out.println(linkValue);
+        }
+
+        return listUrl;
     }
 }
